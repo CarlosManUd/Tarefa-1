4fbc2c84ef307141d171665bf820bd831dfc6073
carli <carli@LAPTOP-6K4VSPGG>
2020-03-31 10:07:56 +0200

    Tarefa 1 realizada

---
+++ Tarefa1/Tarefa_1/src/tarefa_1/Tarefa_1.java
@@ -0,0 +1,18 @@
+package tarefa_1;
+
+/**
+ *
+ * @author carli
+ */
+public class Tarefa_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Componentes vis = new Componentes();
+        
+    }
+    
+}
